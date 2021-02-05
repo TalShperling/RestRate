a6aec3b0a3d1f6501e406774bead515309ae687c
LiorVais <larrymv@gmail.com>
2021-02-05 15:32:59 +0200

    icons and menu

--- app/src/main/java/com/example/restrate/MainActivity.java
+++ app/src/main/java/com/example/restrate/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.restrate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.main_navhost_frag);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            navController.navigateUp();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
